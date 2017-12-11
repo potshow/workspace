e0e645d8fddf33cccaf70837de8074d594542767
potshow <jihyo_hello@naver.com>
2017-12-11 16:30:15 +0900

    java저장

---
+++ java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	// main method(메서드), 프로그램의 시작점 ㅎ ㅎ ㅎ 
+	// string[] args: argument, parameter 라고 부름
+	public static void main (String[] args) {
+		//system.out.println 메서드: 괄호 안의 글자를 출력
+		System.out.println("Hello, World");
+	}
+} 
