8a1931c2b1a7960f3901abf0eda327ff8a719a5f
potshow <jihyo_hello@naver.com>
2017-12-14 10:55:01 +0900

    Day04 실습문제06

---
+++ Day04/problem06/MyJavac.java
@@ -0,0 +1,10 @@
+public class MyJavac {
+
+	public static void main(String[] args) {
+	System.out.println("옵션: " + args[0] + " / " + "값: " + args[1]);
+	System.out.println("옵션: " + args[2] + " / " + "값: " + args[3]);
+		
+		
+	}
+
+}
