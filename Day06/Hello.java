d0d25782de2eeebacd9bdae237156831ce415fe7
potshow <jihyo_hello@naver.com>
2017-12-18 16:46:58 +0900

    Day06 실습문제

--- Day06/Hello.java
+++ Day06/Hello.java
@@ -1,5 +1,49 @@
 public class Hello {
 
+	public static void main(String[] main) {
 
-
+		for (char i = 65; i < 91; i++){
+			
+			if(i == 'H') { 
+		
+			System.out.print(i);
+			break;
+			
+			}
+		
+		}
+		for (char i = 65; i < 91; i++){
+			
+			if(i == 'E') { 
+		
+			System.out.print(i);
+			break;
+			
+			}
+		
+		}
+		for (char i = 65; i < 91; i++){
+			
+			if(i == 'L') { 
+		
+			System.out.print(i);
+			System.out.print(i);
+			break;
+			
+			}
+		
+		}
+		for (char i = 65; i < 91; i++){
+			
+			if(i == 'O') { 
+		
+			System.out.print(i);
+			break;
+			
+			}
+		
+		}
+		
+	}
+	
 }
